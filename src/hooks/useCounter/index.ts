a92c91acfa60e11febb301cc3e7248da5c60bded
Tymek <tymoteusz.luczko@hyd.agency>
2020-07-26 18:53:16 +0200

    amended folder name for tests, added JSDoc and added exclusion condition for Jest

--- src/hooks/useCounter/index.ts
+++ src/hooks/useCounter/index.ts
@@ -1,25 +1,3 @@
-/**
- * Classic counter example to help understand the flow of this npm package
- * Usage:
- * import {useCounter} from from "@neko/react-utility-hooks";
- * const ExampleComponent = () => {
- *   const { count, increment, reset, decrement } = useCounter();
- *   return (
- *   <>
- *     <button onClick={increment}>Increment counter</button>
- *     <button onClick={reset}>Reset counter</button>
- *     <button onClick={decrement}>Decrement counter</button>
- *      <p>{count}</p>
- *    </> )}
- *
- * @param initialValue {number} initial counter value
- *
- * @returns count {number} current counter value
- * @returns increment {fn} to increment count value
- * @returns decrement {fn} to decrement count value
- * @returns reset {fn} to reset count value
- */
-
 import { useCallback, useState } from 'react';
 
 type IUseCounter = {
@@ -29,10 +7,46 @@ type IUseCounter = {
   decrement: () => void;
 };
 
+/**
+ * Classic counter example to help understand the flow of this npm package
+ *
+ * @param    {number} initialValue
+ *           initial counter value
+ *
+ * @return   {Object}
+ *           object with count and methods
+ *
+ * @property {number} count
+ *           The current count state
+ *
+ * @property {()=>void} increment
+ *           the increment function
+ *
+ * @property {()=>void} decrement
+ *           the decrement function
+ *
+ * @property {()=>void} reset
+ *           the reset function
+ *
+ * @example
+ *   const ExampleComponent = () => {
+ *     const { count, increment, reset, decrement } = useCounter();
+ *
+ *     return (
+ *       <>
+ *         <button onClick={increment}>Increment counter</button>
+ *         <button onClick={reset}>Reset counter</button>
+ *         <button onClick={decrement}>Decrement counter</button>
+ *         <p>{count}</p>
+ *       </>
+ *      )
+ *    }
+ */
+
 export const useCounter = (initialValue: number = 0): IUseCounter => {
   const [count, setCount] = useState<number>(initialValue);
-  const increment = useCallback(() => setCount((value: number) => value + 1), []);
-  const decrement = useCallback(() => setCount((value: number) => value - 1), []);
+  const increment = useCallback(() => setCount((value) => value + 1), []);
+  const decrement = useCallback(() => setCount((value) => value - 1), []);
   const reset = useCallback(() => setCount(initialValue), [initialValue]);
   return { count, increment, decrement, reset };
 };
