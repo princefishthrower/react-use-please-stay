6bf9d4d7e6d19c280105b8b89092ab7197d274e7
Tymek <tymoteusz.luczko@hyd.agency>
2020-07-26 21:03:01 +0200

    extracted to a helper

--- src/hooks/useCounter/index.ts
+++ src/hooks/useCounter/index.ts
@@ -9,6 +9,37 @@ type IUseCounter = {
 };
 
 /**
+ * The validator function returns
+ * - input number for number or strings that can be parsed to a number
+ * - 0 for other
+ *
+ * @param {any} initialValue
+ *              Value to be validated
+ *
+ * @return {number}
+ *         input number or 0 for wrong input
+ *
+ * @example
+ *        const validatedInitialValue = validateInitialValue(initialValue);
+ */
+const validateInitialValue = (initialValue: any) => {
+  if (typeof initialValue === 'string') {
+    console.log(
+      'you have passed a string to useCounter. It still may work however. Please pass a number.',
+    );
+    initialValue = parseInt(initialValue);
+  }
+
+  if (isNaN(initialValue)) {
+    console.log(
+      'you really want to break the useCounter. Please pass a number as parameter. Defaulting to zero.',
+    );
+    initialValue = 0;
+  }
+  return initialValue;
+};
+
+/**
  * Classic counter example to help understand the flow of this npm package
  *
  * @param    {number} initialValue
@@ -45,22 +76,14 @@ type IUseCounter = {
  */
 
 export const useCounter = (initialValue: number = 0): IUseCounter => {
-  if (typeof initialValue === 'string') {
-    console.log('you have passed a string to useCounter. It still may work however');
-    initialValue = parseInt(initialValue);
-  }
+  const validatedInitialValue = validateInitialValue(initialValue);
 
-  if (isNaN(initialValue)) {
-    console.log(
-      'you really want to break the useCounter. Please pass a number as parameter. Defaulting to zero.',
-    );
-    initialValue = 0;
-  }
-
-  const [count, setCount] = useState<number>(initialValue);
+  const [count, setCount] = useState<number>(validatedInitialValue);
   const increment = useCallback(() => setCount((value) => value + 1), []);
   const decrement = useCallback(() => setCount((value) => value - 1), []);
-  const reset = useCallback(() => setCount(initialValue), [initialValue]);
+  const reset = useCallback(() => setCount(validatedInitialValue), [
+    validatedInitialValue,
+  ]);
   return { count, increment, decrement, reset };
 };
 
