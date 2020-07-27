b86c073f67bcd1542dbd79242640793856f94b55
Tymek <tymoteusz.luczko@hyd.agency>
2020-07-27 07:10:30 +0200

    small copy change

--- src/helpers/validateInitialValue.ts
+++ src/helpers/validateInitialValue.ts
@@ -15,14 +15,14 @@
 export const validateInitialValue = (initialValue: any) => {
   if (typeof initialValue === 'string') {
     console.log(
-      'you have passed a string to useCounter. It still may work however. Please pass a number.',
+      'you have passed a string when a number is required. It still may work however. Please pass a number.',
     );
-    initialValue = parseInt(initialValue);
+    initialValue = parseInt(initialValue, 10);
   }
 
   if (isNaN(initialValue)) {
     console.log(
-      'you really want to break the useCounter. Please pass a number as parameter. Defaulting to zero.',
+      'you really want to break the validation. Please pass a number as parameter. Defaulting to zero.',
     );
     initialValue = 0;
   }
